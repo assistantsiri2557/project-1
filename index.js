602dc375ceb2038dd4ba2758be4679338eb59f4f
assistantsiri2557 <139713266+assistantsiri2557@users.noreply.github.com>
2023-07-22 22:50:40 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+let PersonName = "Muhammad Huzaifa";
+let income = PersonName.toLowerCase();
+console.log(income);
---
+++ index.ts
@@ -0,0 +1,5 @@
+let PersonName: string = "Muhammad Huzaifa"
+let income = PersonName.toLowerCase();
+console.log(income)
+
+
